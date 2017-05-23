dba8be792c4c8963e84b0deabd13a4ff4ec87bf6
eadaradhiraj <eadaradhiraj@gmail.com>
2017-05-23 16:36:00 +0200

    Update getjobs.py

--- getjobs.py
+++ getjobs.py
@@ -1,21 +1,18 @@
 import sys
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, NoSuchWindowException, WebDriverException
+from selenium.common.exceptions import NoSuchElementException, NoSuchWindowException
 from bs4 import BeautifulSoup
 import time
 
 lang = ['You fluently speak English and German']
-chromedriver = "./chromedriver"
+
 Soup = lambda htm: BeautifulSoup(htm, 'html.parser')
 
 
 def gethtmjs(skill, time_period):
     URL = "https://www.thelocal.de/jobs/Berlin/all/" + skill + "?date_posted=" + time_period
     try:
-        try:
-            driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
-        except WebDriverException:
-            driver = webdriver.Chrome(chromedriver)
+        driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
         driver.get(URL)
 
         while True:
@@ -33,21 +30,22 @@ def gethtmjs(skill, time_period):
 
 
 def getjobs():
-    listofjobs=[]
-    soup = Soup(gethtmjs(skill='python', time_period='day'))
-    for lnk in soup.find_all('a', {"class": "standardJobUrl"}):
-        if ('data' in lnk.text or 'Data' in lnk.text or 'BI' in lnk.text or 'Business' in lnk.text or 'business' in lnk.text or 'analytics' in lnk.text or 'Analytics' in lnk.text or 'analyst' in lnk.text or 'Analyst' in lnk.text):
-            if 'senior' not in lnk.text and 'Senior' not in lnk.text and 'lead' not in lnk.text and 'Lead' not in lnk.text:
-                listofjobs.append(lnk['href'])
 
-    soup = Soup(gethtmjs(skill='sql', time_period='day'))
-    for lnk in soup.find_all('a', {"class": "standardJobUrl"}):
-        if ('data' in lnk.text or 'Data' in lnk.text or 'BI' in lnk.text or 'Business' in lnk.text or 'business' in lnk.text or 'analytics' in lnk.text or 'Analytics' in lnk.text or 'analyst' in lnk.text or 'Analyst' in lnk.text):
-            if 'senior' not in lnk.text and 'Senior' not in lnk.text and 'lead' not in lnk.text and 'Lead' not in lnk.text:
-                listofjobs.append(lnk['href'])
+    jobs = []
 
-    listofjobs = list(set(listofjobs))
-    for e in listofjobs:
-        print(e)
+    '''
+    soup = Soup(gethtmjs(skill='python', time_period='week'))
+    for lnk in soup.find_all('a', {"class": "standardJobUrl"}):
+        if ('data' in lnk.text or 'Data' in lnk.text or 'BI' in lnk.text or 'Business' in lnk.text or 'business' in lnk.text or 'analytics' in lnk.text or 'Analytics' in lnk.text):
+            if 'senior' not in lnk.text and 'Senior' not in lnk.text and 'lead' not in lnk.text and 'Lead' not in lnk.text:
+                jobs.append(lnk['href'])
+    '''
+
+    soup = Soup(gethtmjs(skill='sql', time_period='week'))
+    for lnk in soup.find_all('a', {"class": "standardJobUrl"}):
+        if 'business' in lnk.text or 'Business' in lnk.text or 'report' in lnk.text or 'Report' in lnk.text:
+            if 'Lead' not in lnk.text and 'lead' not in lnk.text and 'Senior' not in lnk.text and 'senior' not in lnk.text:
+                print(lnk['href'])
+
 
 getjobs()
