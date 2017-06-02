524a370e6f1a8c8d5cb9e1179d0c0558a1e30edc
eadaradhiraj <eadaradhiraj@gmail.com>
2017-06-02 22:13:59 +0200

    Update ebooksearcher.py

--- ebooksearcher.py
+++ ebooksearcher.py
@@ -8,7 +8,6 @@ import time
 import os
 import traceback
 import fileDownloader
-import downloads
 from bs4 import BeautifulSoup
 from google import search
 
@@ -21,28 +20,6 @@ def Soup(htm):
 def _dwnfil(url, file_name):
     print(url)
     fileDownloader.DownloadFile(url=url, localFileName=file_name).download()
-    #downloads.download(url=url, out_path=file_name, progress=True)
-    '''
-    u = urllib2.urlopen(url)
-    f = open(file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8) * (len(status) + 1)
-        print status,
-    f.close()
-    '''
-
 
 # The following acquires the html source code using urllib2
 def gethtml(url):
